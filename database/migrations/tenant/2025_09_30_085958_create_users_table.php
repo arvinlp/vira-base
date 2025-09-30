79383e70a74c40870781b38d91e4bb19abded0a7
arvinlp <arvinlp91@gmail.com>
2025-09-30 14:12:04 +0330

    Add 'code' column to users table migration

--- database/migrations/tenant/2025_09_30_085958_create_users_table.php
+++ database/migrations/tenant/2025_09_30_085958_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
 
+            $table->string('code')->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('mobile')->unique()->nullable();
